b45a9dc92e3dddf25b65fde4cea6f65d24eb45d7
SKCC <SKCC@SKCC16D00066.SKCC.NET>
2020-07-06 20:46:12 +0900

    first commit

---
+++ mypage/src/main/java/shopid/MypageViewHandler.java
@@ -0,0 +1,88 @@
+package shopid;
+
+import shopid.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MypageViewHandler {
+
+
+    @Autowired
+    private MypageRepository mypageRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenOrdered_then_CREATE_ (@Payload Ordered ordered) {
+        try {
+            if (ordered.isMe()) {
+                // view 객체 생성
+                Mypage mypage = new Mypage();
+                // view 객체에 이벤트의 Value 를 set 함
+                mypage.setOrderId(ordered.getId());
+                mypage.setProdId(ordered.getProductId());
+                mypage.setQty(ordered.getQty());
+                // view 레파지 토리에 save
+                mypageRepository.save(mypage);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenShipped_then_UPDATE_(@Payload Shipped shipped) {
+        try {
+            if (shipped.isMe()) {
+                // view 객체 조회
+                List<Mypage> mypageList = mypageRepository.findByOrderId(shipped.getOrderId());
+                for(Mypage mypage : mypageList){
+                    // view 객체에 이벤트의 eventDirectValue 를 set 함
+                    mypage.setDeliveryId(shipped.getId());
+                    mypage.setStatus(shipped.getStatus());
+                    mypage.setOrderId(shipped.getOrderId()); // convert to long
+                    // view 레파지 토리에 save
+                    mypageRepository.save(mypage);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenDeliveryCanceled_then_UPDATE_(@Payload DeliveryCanceled deliveryCanceled) {
+        try {
+            if (deliveryCanceled.isMe()) {
+                // view 객체 조회
+                List<Mypage> mypageList = mypageRepository.findByOrderId(deliveryCanceled.getOrderId());
+                for(Mypage mypage : mypageList){
+                    // view 객체에 이벤트의 eventDirectValue 를 set 함
+                    mypage.setStatus(deliveryCanceled.getStatus());
+                    mypage.setOrderId(deliveryCanceled.getOrderId()); // convert to long
+                    // view 레파지 토리에 save
+                    mypageRepository.save(mypage);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+//    @StreamListener(KafkaProcessor.INPUT)
+//    public void when_then_DELETE_(@Payload  ) {
+//        try {
+//            if (.isMe()) {
+//                // view 레파지 토리에 삭제 쿼리
+//                mypageRepository.deleteBy(.get());
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+}
