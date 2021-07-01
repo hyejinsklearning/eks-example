60ead335767c5cdfabcf0552362c1bd59661486f
HyejinLee <hyejin.lee@sk.com>
2021-07-01 05:48:19 +0000

    sample appl

--- delivery/src/main/java/shopid/CancellationRepository.java
+++
@@ -1,8 +0,0 @@
-package shopid;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
-
-
-}
--- delivery/src/main/java/shopid/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package shopid;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
-
-
-}
